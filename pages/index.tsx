32cb3fd743ab9d9d21a26e375cda7cb5be96060b
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-07-04 00:56:23 +0530

    " ' " error fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Arjun Aghera's Portfolio</title>
+        <title>{`Arjun Aghera's Portfolio`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className=" bg-1">
